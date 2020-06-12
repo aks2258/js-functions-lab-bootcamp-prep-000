d4c2c3f5cb358d8a300f19e0b33d44b6b55ae5fc
Arnie Serrano <arniekidd@gmail.com>
2020-06-12 03:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(daysdays, holiday){
+function holidayCountdown(days, holiday){
   return `It's ${days} days until ${holiday}!`
 }
